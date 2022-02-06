93eec847b782757f4429b7d1c4682542a3039023
Rotimi Bolorunduro <rotimibolorunduro@gmail.com>
2022-02-06 18:53:46 +0100

    Refactored to answer test question better

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,55 +1,93 @@
 import React, { useEffect, useState } from 'react';
-import { JatodianSEs } from "../data/jatado-se-team";
+import { filenames } from "../data/filenames";
 
 export default function Search() {
-  
       const [searchKeyword, setSearchKeyword] = useState('');
-      const [team, setTeam] = useState([]);
+      const [filename, setFileName] = useState([]);
 
       const handleChange = (e) => {
         setSearchKeyword(e.target.value);
       };
 
       useEffect(() => {
-        setTeam(JatodianSEs);
+        setFileName(filenames);
       }, []);
 
-      let JSEteam = team;
+      let files = filename;
       let search = searchKeyword.trim().toLowerCase();
   
       if (search.length > 0) {
-        JSEteam = JSEteam.filter(function(team) {
-          return team.name.toLowerCase().match(search);
+        var searchWord = files.filter(function(files) {
+          return files.filename.toLowerCase().match(search);
         });
+
+        var searchchars = search.split("");
+        const searchCharFile = [];
+        
+        searchchars.forEach(element => {
+          const searchCharFiles = files.filter(function(files) {
+            return files.filename.toLowerCase().match(element)
+          });
+          searchCharFile.push(searchCharFiles);
+        });
+
+        var searchChar = Object.entries(
+          Object.fromEntries(
+            searchCharFile.flat(1)
+            .map(v => [v.filename, v.description])))
+            .map(([filename, description]) => ({filename, description}));
+
+        var searchOther = searchChar.filter(a => !searchWord.map(b=>b.filename).includes(a.filename));
       }
   
       return (
         <div>
-          <h1>Search For The Position of A Jatodian Software Engineer!</h1>
+          <h1>Search Box!</h1>
           <div>
             <input
               type="text"
+              maxLength={10}
               value={searchKeyword}
               onChange={handleChange}
-              placeholder="Search for a Jatado Engineer"
+              placeholder="Search for a File"
             />
-            {search.length && JSEteam.length ? 
+            {search.length && files.length ? 
             (<div>
-                    {JSEteam.map((engineer, index) => {
+              {searchWord.length ? (<div className="card purple-card">
+                {searchWord.length} Perfect Match Found!
+              </div>) : (<></>)}
+                    {searchWord.map((file, index) => {
                         return (
                           <div key={index}>
                             <div className="card">
-                                <span className="mood">{engineer.mood}</span>
-                                <span className="name">{engineer.name}</span>
-                                <span className="position">{engineer.position}</span>
+                                <span className="icon">📂</span>
+                                <span className="name">{file.filename}</span> <span className="name-info">(Perfect Match: contains exact string "{search}")</span>
+                                <span className="desc">{file.description}</span>
+                            </div>
+                          </div>
+                        );
+                    })}
+                    {searchOther.length ? (<div className="card purple-card">
+                      {searchOther.length} Occurrences Found!
+                    </div>) : (<></>)}
+                    {searchOther.map((file, index) => {
+                        return (
+                          <div key={index}>
+                            <div className="card red-card">
+                                <span className="icon">📂</span>
+                                <span className="name">{file.filename}</span> <span className="name-info">(Other Matches: contains any character in "{searchchars}")</span>
+                                <span className="desc">{file.description}</span>
                             </div>
                           </div>
                         );
                     })}
             </div>)
-            : search.length && !JSEteam.length ? ('No Match Found!')
+            : search.length && !files.length ? ('No Match Found!')
             :(
-                'Are you a Jatado Engineer ? Yes! Search for your Position using your name.'
+                <div className="card purple-card">
+                  Assuming you stored your files using each day of the week as the file name i.e Monday, Tuesday, and so on.
+                  Search for a file using a certain day of the week.
+                </div>
             )}
           </div>
         </div>
